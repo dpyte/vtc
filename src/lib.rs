0c1dd3e1ea4295a23ed5e7555039201305e0c6e1
Dhruv Patel <pateldrv456@gmail.com>
2024-10-17 00:21:35 -0400

    Add serialization to ArtifactExecConfig and package enhancements
    Implemented the Serialize trait for ArtifactExecConfig enabling it to be serialized. This update also includes various enhancements in build configurations, dependencies, and package handling functionalities.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 #![allow(dead_code)]
 
+use std::rc::Rc;
+use crate::value::Value;
+
 pub mod parser;
 pub mod runtime;
 pub mod cffi;
 pub mod value;
 
 pub const SMALL_VEC_SIZE: usize = 64;
+
+
+pub type VtcFnArg = Vec<Rc<Value>>;
+pub type VtcFnRet = Rc<Value>;

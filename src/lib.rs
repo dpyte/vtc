24676669f5eee6829f5a285296bac12a11706fd9
Dhruv Patel <pateldrv456@gmail.com>
2024-12-30 22:55:53 -0500

    increase small_vec_size to 512

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,12 @@ use std::sync::Arc;
 
 use crate::value::Value;
 
+pub mod cffi;
 pub mod parser;
 pub mod runtime;
-pub mod cffi;
 pub mod value;
 
-pub const SMALL_VEC_SIZE: usize = 128;
+pub const SMALL_VEC_SIZE: usize = 512;
 
 pub type VtcFnArg = Vec<Arc<Value>>;
 pub type VtcFnRet = Arc<Value>;
